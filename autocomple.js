33417a8fa6a7f73f545fb4841a809ad07af57bcf
Antonius Gotama <antoniusgotama1321@gmail.com>
2023-08-07 16:30:33 +0700

    Added onclick event jump to link

--- autocomple.js
+++ autocomple.js
@@ -1,24 +1,31 @@
 const availableKeywords = [
     {   nama: "MA3131 Pengantar Analisis Real",
-        singkatan: "penganril"
+        singkatan: "penganril",
+        link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6flRYZkl3M3N3d25NMVM0dGdfdzlENUtPRXIyLUgyY0VhdlItTGtvb2RCbUU?resourcekey=0-34_L6IthvotqH1RcYHV8cA"
     },
     {   nama:  "AK3283 Model Risiko I",
-        singkatan: "modris"
+        singkatan: "modris",
+        link: "https://drive.google.com/drive/u/1/folders/19pOSQxlABIJ9a60Ef2u-v3ry8_uH9cck"
     },
     {   nama: "MA3281 Statistika Matematika",
-        singkatan: "statmat"
+        singkatan: "statmat",
+        link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6fmRoMnFZSjVBQkRPTWMySjZtMFY3Q3RnVVc1VFJmSkRtVjRiRTdMUWtabTQ?resourcekey=0-NCOyvfzfGrSwAyb1Oa0EVw"
     },
     {   nama: "AK2264 Akuntansi",
-        singkatan: "akun"
+        singkatan: "akun",
+        link: "https://drive.google.com/drive/u/1/folders/1_3N1D7BCcYLTOUwRStiIzJMjQACB5c3W"
     },
     {   nama: "MA2181 Analisis Data",
-        singkatan: "andat"
+        singkatan: "andat",
+        link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6fmpqd2VVbnhEUjMwRkVycXJicFBrd0xKTVZxc0hFUnZqc1E3c0NGOWNHRHM?resourcekey=0-5pC8hXzhEV7FsYkunrFVEw"
     },
     {   nama: "MA2151 Simulasi dan Komputasi Matematika",
-        singkatan: "sikomat"
+        singkatan: "sikomat",
+        link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6fmxQNmg1RzVxX1BTaEU4SWJIczhBYV9rZGFnTklMdHBnSTNGLUJtTExnOGM?resourcekey=0-jpvmkp84GbsmMbNHsdEMNw"
     },
     {   nama: "MA2271 Pengantar Persamaan Diferensial",
-        singkatan: "ppd"
+        singkatan: "ppd",
+        link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6fldKTGFRSzJPR0E5Z0Y1QjZGdzEwYVIwRTNvU04zdjVnVWJnRDhJWlVsb2c?resourcekey=0-R4FGUa99J2oxUBdk9ENEUQ"
     },
   ];
 
@@ -34,7 +41,7 @@ inputBox.onkeyup = function(){
         });
         console.log(result);
     }
-    display(result.map((item) => item.nama));
+    display(result.map((item) => [item.nama, item.link]));
 
     if(!result.length){
         resultsBox.innerHTML = "";
@@ -43,13 +50,15 @@ inputBox.onkeyup = function(){
 
 function display(result){
     const content = result.map((list)=>{
-        return `<li onclick=selectInput(this)>${list}</li>`;
+        return `<li onclick=jumpToLink("${list[1]}")> ${list[0]}</li>`;
+
     });
 
     resultsBox.innerHTML = `<ul>${content.join('')}</ul>`;
 }
 
-function selectInput(list){
-    inputBox.value = list.innerHTML;
+function jumpToLink(link) {
+    window.location.href = link;
     resultsBox.innerHTML = "";
 }
+  
