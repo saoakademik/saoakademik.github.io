47ba0f8c6c862dc24a5d2ecd1a9798dfb7b5c3c5
Kevin Christ Aditya <123429558+HappiHinoka@users.noreply.github.com>
2023-10-06 09:08:17 +0700

    test 1 by kace

--- autocomple.js
+++ autocomple.js
@@ -1,38 +1,13 @@
 const availableKeywords = [
   {
-    nama: "MA3131 Pengantar Analisis Real",
-    singkatan: "penganril",
-    link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6flRYZkl3M3N3d25NMVM0dGdfdzlENUtPRXIyLUgyY0VhdlItTGtvb2RCbUU?resourcekey=0-34_L6IthvotqH1RcYHV8cA",
+    nama: "MA2011 - Topik Wawasan Matematika",
+    singkatan: "topwaw",
+    link: "https://drive.google.com/drive/folders/1GC5kEEcruSPcWZYwvcUsxqxCkt70gDbN?usp=drive_link",
   },
   {
-    nama: "AK3283 Model Risiko I",
-    singkatan: "modris",
-    link: "https://drive.google.com/drive/u/1/folders/19pOSQxlABIJ9a60Ef2u-v3ry8_uH9cck",
-  },
-  {
-    nama: "MA3281 Statistika Matematika",
-    singkatan: "statmat",
-    link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6fmRoMnFZSjVBQkRPTWMySjZtMFY3Q3RnVVc1VFJmSkRtVjRiRTdMUWtabTQ?resourcekey=0-NCOyvfzfGrSwAyb1Oa0EVw",
-  },
-  {
-    nama: "AK2264 Akuntansi",
-    singkatan: "akun",
-    link: "https://drive.google.com/drive/u/1/folders/1_3N1D7BCcYLTOUwRStiIzJMjQACB5c3W",
-  },
-  {
-    nama: "MA2181 Analisis Data",
-    singkatan: "andat",
-    link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6fmpqd2VVbnhEUjMwRkVycXJicFBrd0xKTVZxc0hFUnZqc1E3c0NGOWNHRHM?resourcekey=0-5pC8hXzhEV7FsYkunrFVEw",
-  },
-  {
-    nama: "MA2151 Simulasi dan Komputasi Matematika",
-    singkatan: "sikomat",
-    link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6fmxQNmg1RzVxX1BTaEU4SWJIczhBYV9rZGFnTklMdHBnSTNGLUJtTExnOGM?resourcekey=0-jpvmkp84GbsmMbNHsdEMNw",
-  },
-  {
-    nama: "MA2271 Pengantar Persamaan Diferensial",
-    singkatan: "ppd",
-    link: "https://drive.google.com/drive/u/1/folders/0B20OGwmrcxo6fldKTGFRSzJPR0E5Z0Y1QjZGdzEwYVIwRTNvU04zdjVnVWJnRDhJWlVsb2c?resourcekey=0-R4FGUa99J2oxUBdk9ENEUQ",
+    nama: " ",
+    singkatan: " ",
+    link: " ",
   },
 ];
 
